35e77a27ad07f0bb76658c7c63ae589b56aaccf7
pathymavuba <mavubapathy@gmail.com>
2023-06-14 12:24:11 +0100

    develop:page chat is done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import socketI0 from "socket.io-client"
 import Home from "./pages/Home";
+import ChatPage from "./pages/Chat";
 // eslint-disable-next-line no-unused-vars
 const socket = socketI0.connect("http://localhost:4000")
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/"  element={<Home/>} />
+      <Route path ="/chat" element={<ChatPage socket={socket} />} />
     </Routes>
   );
 }
